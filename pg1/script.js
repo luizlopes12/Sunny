89c7f59d8cc7b7cab309d06ac929519ee6a1b345
Luiz Lopes <luizlopes12@outlook.com>
2021-01-22 20:03:12 -0300

    alguns detalhes na responsividade
    mudei o padding dos itens utilizando porcentagem

--- pg1/script.js
+++ pg1/script.js
@@ -1,18 +1,1 @@
-function inputvazio(){
-    let inputvazio1 = document.getElementById('email')
-    let inputvazio2 = document.getElementById('senha')
-
-    if(inputvazio1.value.length == 0 || inputvazio2.value.length == 0){
-        let blockLink = document.getElementsByTagName('a')[0]
-        blockLink.removeAttribute('href')
-    }
-    //resolver
-   /* 
-   else if(inputvazio1.value.length > 0 || inputvazio2.value.length > 0) {
-        let link = blockLink.createAttribute('href')
-        link.value = 'pg-andamento/andamento.html'
-        
-    }
-    */
-}
 
