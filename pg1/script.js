c12f18c81873d7240ff4862adf535e42e199c17e
Luiz Lopes <luizlopes12@outlook.com>
2021-02-01 10:18:00 -0300

    Update script.js

--- pg1/script.js
+++ pg1/script.js
@@ -1,1 +1,17 @@
+/*check(){
+    let email = document.getElementsByClassName('login')
+    let senha = document.getElementsByClassName('senha')
+    let entrar = document.getElementsByClassName('entrar')
+
+    if(email.length == 0 || senha.length == 0){
+        entrar = window.location.href = '';
+    }
+    else{
+        entrar = window.location.href = '../pg-andamento/andamento.html';
+    }
+
+
+
+
+}*/
 
